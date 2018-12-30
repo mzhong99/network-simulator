bd42ac70444c91ded7f6ba89f873ab67ce04e558
tarediiran <mzhong99@vt.edu>
2018-12-29 21:57:51 -0500

    First commit. Created Grid data structure and testing apparatus

---
+++ Grid.java
@@ -0,0 +1,110 @@
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+
+import java.lang.IllegalArgumentException;
+
+public class Grid {
+    
+    private List<List<Tile>> grid;
+    private Set<Tile> validTiles;
+
+    private final int width;
+    private final int height;
+    
+    private Tile getTileAt(int row, int col) {
+
+        if (row < 0 || row >= height || col < 0 || col >= width) {
+            throw new IllegalArgumentException("Attempted to access tile out of bounds");
+        }
+
+        return grid.get(row).get(col);
+    }
+
+    public Grid(int width, int height) {
+        
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Width and height less than zero");
+        }
+        
+        this.width  = width;
+        this.height = height;
+        
+        this.grid = new ArrayList<List<Tile>>();
+        
+        for (int r = 0; r < height; r++) {
+            
+            List<Tile> line = new ArrayList<Tile>();
+            
+            for (int c = 0; c < width; c++) {
+                line.add(new Tile(r, c, this));
+            }
+            
+            grid.add(line);
+        }
+
+        this.validTiles = new HashSet<Tile>();
+    }
+    
+    public int getWidth()  { return width;  }
+    public int getHeight() { return height; }
+    
+    public void setAttributeAt(int row, int col, String attribute) {
+        
+        if (row < 0 || row >= height || col < 0 || col >= width) {
+            throw new IllegalArgumentException("Attempted to set out of bounds");
+        }
+        
+        String oldAttribute = this.getAttributeAt(row, col);
+        boolean isTurnedOn  =  ("".equals(oldAttribute)) && !("".equals(attribute));
+        boolean isTurnedOff = !("".equals(oldAttribute)) &&  ("".equals(attribute));
+
+        grid.get(row).get(col).setAttribute(attribute);
+        
+        if (isTurnedOn) {
+            validTiles.add(getTileAt(row, col));
+        }
+
+        if (isTurnedOff) {
+            validTiles.remove(getTileAt(row, col));
+        }
+    }
+
+    public String getAttributeAt(int row, int col) {
+
+        if (row < 0 || row >= height || col < 0 || col >= width) {
+            throw new IllegalArgumentException("Attempted to access attribute out of bounds");
+        }
+
+        return getTileAt(row, col).getAttribute();
+    }
+
+    public Set<Tile> getValidTiles() {
+        
+        Set<Tile> validTilesCopy = new HashSet<Tile>();
+
+        for (Tile tile : validTiles) {
+            validTilesCopy.add(new Tile(tile));
+        }
+
+        return validTilesCopy;
+    }
+
+    public Map<Tile, List<Tile>> getAdjacencyMap() {
+        
+        Map<Tile, List<Tile>> adjacency = new HashMap<Tile, List<Tile>>();
+        
+        for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                Tile cursor = getTileAt(r, c);
+                adjacency.put(cursor, cursor.getNeighbors());
+            }
+        }
+        
+        return adjacency;
+    }
+}
---
+++ Tile.java
@@ -0,0 +1,102 @@
+import java.util.List;
+import java.util.ArrayList;
+
+import java.lang.IllegalArgumentException;
+
+public class Tile {
+    
+    private final int row;
+    private final int col;
+    private final Grid grid;
+    private String attribute;
+
+    private boolean isInRange(Tile tile) {
+        return tile.row >= 0 && tile.row < grid.getHeight()
+            && tile.col >= 0 && tile.col < grid.getWidth();
+    }
+
+    private boolean isTraversable(Tile tile) {
+        if (tile == null) {
+            return false;
+        }
+        return !("".equals(tile.attribute));
+    }
+
+    public Tile(int row, int col, Grid grid) {
+        this(row, col, grid, "");
+    }
+
+    public Tile(int row, int col, Grid grid, String attribute) {
+        
+        this.row = row;
+        this.col = col;
+
+        if (grid == null) {
+            throw new IllegalArgumentException("Tile received null grid");
+        }
+
+        this.grid = grid;
+        this.attribute = attribute;
+    }
+
+    public Tile(Tile rhs) {
+        this.row = rhs.row;
+        this.col = rhs.col;
+        this.grid = rhs.grid;
+        this.attribute = rhs.attribute;
+    }
+
+    public int getRow()          { return this.row;       }
+    public int getCol()          { return this.col;       }
+    public String getAttribute() { return this.attribute; }
+
+    public void setAttribute(String attribute) { this.attribute = attribute; }
+
+    private Tile up()    { return new Tile(row - 1, col, grid); }
+    private Tile down()  { return new Tile(row + 1, col, grid); }
+    private Tile left()  { return new Tile(row, col - 1, grid); }
+    private Tile right() { return new Tile(row, col + 1, grid); }
+    
+    public List<Tile> getNeighbors() {
+        
+        if ("".equals(attribute)) {
+            return new ArrayList<Tile>();
+        }
+
+        List<Tile> neighborsRaw      = new ArrayList<Tile>();
+        List<Tile> neighborsFiltered = new ArrayList<Tile>();
+
+        neighborsRaw.add(this.up()   );
+        neighborsRaw.add(this.down() );
+        neighborsRaw.add(this.left() );
+        neighborsRaw.add(this.right());
+
+        for (Tile tile : neighborsRaw) {
+            if (isInRange(tile)) {
+                neighborsFiltered.add(tile);
+            }
+        }
+
+        neighborsRaw      = neighborsFiltered;
+        neighborsFiltered = new ArrayList<Tile>();
+
+        for (Tile tile : neighborsRaw) {
+            tile.setAttribute(grid.getAttributeAt(tile.getRow(), tile.getCol()));
+            if (isTraversable(tile)) {
+                neighborsFiltered.add(tile);
+            }
+        }
+
+        return neighborsFiltered;
+    }
+
+    @Override
+    public int hashCode() {
+        return (row << 16) + col;
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        return row == ((Tile)rhs).row && col == ((Tile)rhs).col;
+    }
+}
